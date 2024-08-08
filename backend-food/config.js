3c069c09f1573818c2f55b1fd1598dbe8b593cfa
AleezaMahsood <145749158+AleezaMahsood@users.noreply.github.com>
2024-08-08 17:29:01 +0500

    Update config.js

--- backend-food/config.js
+++ backend-food/config.js
@@ -1,3 +1,1 @@
-export const PORT=5555;
-export const mongoDBURL=
-'mongodb+srv://mahsood4404568:root123@bookstore.goegerg.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore';
+
